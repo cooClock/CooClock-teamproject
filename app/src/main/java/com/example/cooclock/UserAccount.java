dba1c63fba86e57a543932ef68bb0b94c47490eb
datakim1201 <gkstjd1201@soongsil.ac.kr>
2023-11-14 22:54:29 +0900

    ADD:firebase

---
+++ app/src/main/java/com/example/cooclock/UserAccount.java
@@ -0,0 +1,4 @@
+package com.example.cooclock;
+
+public class UserAccount {
+}
--- app/src/main/java/com/example/cooclock/signin_page.java
+++ app/src/main/java/com/example/cooclock/signin_page.java
@@ -1,14 +1,58 @@
 package com.example.cooclock;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class signin_page extends AppCompatActivity {
 
+    private FirebaseAuth mFirebaseAuth; // 파이어베이스 인증
+    private DatabaseReference mDatabaseRef; // 실시간 데이터베이스 연동
+    private EditText mEtID, mEtPwd; // 회원가입 입력필드
+    private Button mBtnRegister;    //회원가입 버튼
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin_page);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+        mEtID = findViewById(R.id.et_id);
+        mEtPwd = findViewById(R.id.et_pw);
+        mBtnRegister = findViewById(R.id.btn_register);
+        mBtnRegister.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view)
+            {
+                // 회원가입 처리 시작
+                String strID = mEtID.getText().toString();
+                String strPW = mEtPwd.getText().toString();
+
+                // Firebase Auth 진행
+                mFirebaseAuth.createUserWithEmailAndPassword(strID,strPW).addOnCompleteListener(signin_page.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
