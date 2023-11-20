b044566c47901be440d027bb804bd460a918001b
Yarrly <74090200+eunsoA@users.noreply.github.com>
2023-11-21 03:22:54 +0900

    recipe_write_page 상단 부분 진행 중

--- app/src/main/java/com/example/cooclock/recipe_write_page.java
+++ app/src/main/java/com/example/cooclock/recipe_write_page.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class recipe_write_page extends AppCompatActivity {
 
@@ -14,11 +16,16 @@ public class recipe_write_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_write_page);
+
+
+        LinearLayout get_picture = findViewById(R.id.input_picture);
+        get_picture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v("test","picture");
+            }
+        });
     }
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                         Bundle savedInstanceState){
-//
-//        return inflater.inflate(R.layout.activity_recipe_write_page,container,false);
-//    }
+
+
 }
