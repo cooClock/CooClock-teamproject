0da546202d83418e6eb1e4ecfc4a8a192ec1f5bb
datakim1201 <gkstjd1201@soongsil.ac.kr>
2023-11-26 17:15:45 +0900

    initial

--- app/src/main/java/com/example/cooclock/photo_scan_page.java
+++ app/src/main/java/com/example/cooclock/photo_scan_page.java
@@ -2,13 +2,25 @@ package com.example.cooclock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class photo_scan_page extends AppCompatActivity {
-
+    private EditText mBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_scan_page);
+        Button btn_cam = findViewById(R.id.cam_button);
+        btn_cam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),camera_scan_page.class);
+                startActivity(intent);
+            }
+        });
     }
 }
