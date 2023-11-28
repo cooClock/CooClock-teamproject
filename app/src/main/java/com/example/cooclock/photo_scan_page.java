0ea0686f353aaf72bf9354ee2c94bd4617a58765
Lee jaewon <61585008+elroy03@users.noreply.github.com>
2023-11-28 13:30:55 +0900

    phot_scan_page 구현 완

--- app/src/main/java/com/example/cooclock/photo_scan_page.java
+++ app/src/main/java/com/example/cooclock/photo_scan_page.java
@@ -1,14 +1,73 @@
 package com.example.cooclock;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class photo_scan_page extends AppCompatActivity {
-
+    Uri uri;
+    ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_scan_page);
+        TextView go_camera_scan_page = (TextView) findViewById(R.id.go_camera_scan_page);
+        ImageButton add_ingredient_image = (ImageButton) findViewById(R.id.add_ingredient_image);
+
+        go_camera_scan_page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(photo_scan_page.this, camera_scan_page.class);
+                startActivity(intent);
+            }
+        });
+
+        add_ingredient_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
+    ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == RESULT_OK && result.getData() != null){
+                        uri = result.getData().getData();
+                        try{
+                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                            imageButton.setImageBitmap(bitmap);
+                            imageButton.setVisibility(View.VISIBLE);
+                        }catch (FileNotFoundException e){
+                            e.printStackTrace();
+                        }catch(IOException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+    );
 }
